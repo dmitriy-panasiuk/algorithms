8e2897fcc5bee7e4d7d3e92b9b3728465c2cb40a
global\dpanasiuk <dpanasiuk@sdl.com>
2015-03-24 15:59:48 +0200

    Implemented initial class structure

--- src/SeamCarver.java
+++ src/SeamCarver.java
@@ -1,2 +1,39 @@
 public class SeamCarver {
+    private Picture picture;
+
+    public SeamCarver(Picture picture) {
+        this.picture = picture;
+    }
+
+    public Picture picture() {
+        return this.picture;
+    }
+
+    public int width() {
+        return this.picture.width();
+    }
+
+    public int height() {
+        return this.picture.height();
+    }
+
+    public double energy(int x, int y) {
+        throw new UnsupportedOperationException();
+    }
+
+    public int[] findHorizontalSeam() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int[] findVerticalSeam() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void removeHorizontalSeam(int[] seam) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void removeVerticalSeam(int[] seam) {
+        throw new UnsupportedOperationException();
+    }
 }
