63744a0a5bae958f485f57c061df03b7083a70d8
DPanasiuk <dmitriy.panasyuk@gmail.com>
2015-04-19 21:10:19 +0300

    Created basic class structure

---
+++ src/BaseballElimination.java
@@ -0,0 +1,39 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class BaseballElimination {
+    public BaseballElimination(String filename) {
+        throw new NotImplementedException();
+    }
+
+    public int numberOfTeams() {
+        throw new NotImplementedException();
+    }
+
+    public Iterable<String> teams() {
+        throw new NotImplementedException();
+    }
+
+    public int wins(String team) {
+        throw new NotImplementedException();
+    }
+
+    public int losses(String team) {
+        throw new NotImplementedException();
+    }
+
+    public int remaining(String team) {
+        throw new NotImplementedException();
+    }
+
+    public int against(String team1, String team2) {
+        throw new NotImplementedException();
+    }
+
+    public boolean isEliminated(String team) {
+        throw new NotImplementedException();
+    }
+
+    public Iterable<String> certificateOfElimination(String team) {
+        throw new NotImplementedException();
+    }
+}
