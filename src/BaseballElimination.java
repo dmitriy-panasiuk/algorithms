f0d66d1cb750c8b9f5468cee4313fa47dbe18f08
global\dpanasiuk <dpanasiuk@sdl.com>
2015-04-20 18:17:45 +0300

    intermediate commit

--- src/BaseballElimination.java
+++ src/BaseballElimination.java
@@ -56,7 +56,22 @@ public class BaseballElimination {
     }
 
     public boolean isEliminated(String team) {
-        throw new NotImplementedException();
+        int teamIndex = teams.get(team);
+        for (int i = 0; i < numberOfTeams; i++) {
+            if (wins[teamIndex] + remaining[teamIndex] < wins[i]) {
+                return true;
+            }
+        }
+
+        FlowNetwork network = new FlowNetwork((numberOfTeams * numberOfTeams - numberOfTeams) / 2 + 2 + numberOfTeams);
+        for (int i = 1; i <= (numberOfTeams * numberOfTeams - numberOfTeams) / 2; i++) {
+            network.addEdge(new FlowEdge(0, i, 0.0));
+        }
+        return false;
+    }
+
+    private double gameCapacity(int n) {
+
     }
 
     public Iterable<String> certificateOfElimination(String team) {
@@ -66,6 +81,6 @@ public class BaseballElimination {
     public static void main(String[] args) {
         BaseballElimination b = new BaseballElimination("teams4.txt");
         System.out.println(b.wins("New_York"));
-        System.out.println(b.against("Atlanta", "New_York"));
+        System.out.println(b.isEliminated("New_York"));
     }
 }
