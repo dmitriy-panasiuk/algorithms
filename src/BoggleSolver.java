e72aabfbd4812e00c6fa903aa932afe0a024a04f
Dmytro Panasiuk <dpanasiuk@sdl.com>
2015-12-11 14:07:07 +0200

    Implemented constructor and scoreOf methods

--- src/BoggleSolver.java
+++ src/BoggleSolver.java
@@ -1,8 +1,15 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.TrieST;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class BoggleSolver {
+    TrieST<Integer> dict = new TrieST<>();
+
     public BoggleSolver(String[] dictionary) {
-        throw new NotImplementedException();
+        for (String word : dictionary) {
+            dict.put(word, score(word.length()));
+        }
     }
 
     public Iterable<String> getAllValidWords(BoggleBoard board) {
@@ -10,6 +17,33 @@ public class BoggleSolver {
     }
 
     public int scoreOf(String word) {
-        throw new NotImplementedException();
+        Integer score = dict.get(word);
+        if (score == null) return 0;
+        return score;
+    }
+
+    private int score(int wordLength) {
+        assert wordLength > 0;
+        if (wordLength < 3) return 0;
+        if (wordLength == 3 || wordLength == 4) return 1;
+        if (wordLength == 5) return 2;
+        if (wordLength == 6) return 3;
+        if (wordLength == 7) return 5;
+        return 11;
+    }
+
+    public static void main(String[] args) {
+        In in = new In("test.txt");
+        String[] dictionary = in.readAllStrings();
+        BoggleSolver solver = new BoggleSolver(dictionary);
+        System.out.println(solver.scoreOf("ABL"));
+        /*BoggleBoard board = new BoggleBoard(args[1]);
+        int score = 0;
+        for (String word : solver.getAllValidWords(board))
+        {
+            StdOut.println(word);
+            score += solver.scoreOf(word);
+        }
+        StdOut.println("Score = " + score);*/
     }
 }
