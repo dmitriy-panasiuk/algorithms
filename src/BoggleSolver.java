8e4e35aa66f6e1d180942b8e7c3e11ed7cf224a4
Dmytro Panasiuk <dpanasiuk@sdl.com>
2015-12-11 11:21:38 +0200

    Added basic structure and provided java class

---
+++ src/BoggleSolver.java
@@ -0,0 +1,15 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class BoggleSolver {
+    public BoggleSolver(String[] dictionary) {
+        throw new NotImplementedException();
+    }
+
+    public Iterable<String> getAllValidWords(BoggleBoard board) {
+        throw new NotImplementedException();
+    }
+
+    public int scoreOf(String word) {
+        throw new NotImplementedException();
+    }
+}
